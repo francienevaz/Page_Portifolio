1ffb65da269a6fe6c9ad508bd7988cc43d5c7390
francienevaz <fran.vfraga@gmail.com>
2023-04-23 15:44:57 -0300

    updated

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -1,6 +1,17 @@
+import Avatar from '../img/fran.jpg'
+import '../styles/components/sidebar.sass'
+
 const SideBar= () => {
     return (
-        <div>SideBar</div>
+        <aside id="sidebar">
+            <img src={Avatar} alt='Franciene Vaz'/>
+            <p className="title">Desenvolvedora</p>
+            <p>redes sociais</p>
+            <p>informações de contato</p>
+            <a href="#" className="btn">
+                Download curriculo
+            </a>
+        </aside>
     )
 }
 
